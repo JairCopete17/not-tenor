e151d4a32cb85a4b2aedc4c364d76cfc6aabc7ba
Jair <jcopeteroys@gmail.com>
2021-12-15 23:46:12 -0500

    Add infinity scroll

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,4 +1,9 @@
+import debounce from 'just-debounce-it'
+import { useCallback, useRef, useEffect } from 'react'
+
+
 import { useGifs } from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
 
 import Spinner from 'components/Spinner'
 import ListOfGifs from 'components/ListOfGifs'
@@ -6,10 +11,19 @@ import ListOfGifs from 'components/ListOfGifs'
 export default function SearchResults ({ params }) {
   const { keyword } = params
 	const { loading, gifs, setPage } = useGifs({ keyword })
+  const externalRef = useRef()
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false
+  })
 
-  const handleNextPage = () => {
-    setPage(prevPage => prevPage + 1)
-  }
+  const debounceHandleNextPage = useCallback(debounce(
+    () => setPage(prevPage => prevPage + 1), 200
+  ), [setPage])
+
+  useEffect(function () {
+    if (isNearScreen) debounceHandleNextPage()
+  }, [debounceHandleNextPage, isNearScreen])
 
   return <>
     {loading
@@ -17,8 +31,8 @@ export default function SearchResults ({ params }) {
       : <>
           <h3 className="App-title">{decodeURI(keyword)}</h3>
           <ListOfGifs gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
         </>
     }
-    <button onClick={handleNextPage}>Get next page</button>
   </>
 }
