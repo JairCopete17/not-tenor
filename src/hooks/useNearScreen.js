87097c43a311e0d7dd2c1247b842aa16f3a90298
Jair <jcopeteroys@gmail.com>
2021-12-17 23:40:23 -0500

    Add IntersectionObserver polyfill

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -9,24 +9,31 @@ export default function useNearScreen ({ distance = '100px', externalRef, once =
 
 		const element = externalRef ? externalRef.current : fromRef.current
 
-		const onChange = (entries, observer) => {
-			const el = entries[0]
-			if (el.isIntersecting) {
-				setShow(true)
-				once && observer.disconnect()
-			} else {
-				!once && setShow(false)
-			}
-		}
+    const onChange = (entries, observer) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+        once && observer.disconnect()
+      } else {
+        !once && setShow(false)
+      }
+    }
 
-		observer = new IntersectionObserver(onChange, {
-			rootMargin: distance
-		})
+    Promise.resolve(
+      typeof IntersectionObserver !== 'undefined'
+        ? IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: distance
+      })
 
-		if (element) observer.observe(element)
+      observer.observe(fromRef.current)
+      if (element) observer.observe(element)
+    })
 
-		return () => observer.disconnect()
-	})
-
-	return {isNearScreen, fromRef}
+    return () => observer && observer.disconnect()
+  })
+  return {isNearScreen, fromRef}
 }
