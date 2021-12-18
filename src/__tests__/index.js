ba9570137c7686e25c4880ca628049bc67509760
Jair <jcopeteroys@gmail.com>
2021-12-18 01:09:26 -0500

    Add tests

---
+++ src/__tests__/index.js
@@ -0,0 +1,20 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import App from '../App';
+
+test('logo renders properly', async () => {
+	render(<App />)
+	const logo = await screen.findByAltText(/Nottenor logo/i)
+	expect(logo).toBeInTheDocument()
+})
+
+test('search form could be used', async () => {
+  render(<App />)
+  const input = await screen.findByRole('textbox')
+  const button = await screen.findByRole('button')
+
+  fireEvent.change(input, { target: { value: 'Matrix' }})
+  fireEvent.click(button)
+
+  const title = await screen.findByText('Matrix')
+  expect(title).toBeVisible()
+})
